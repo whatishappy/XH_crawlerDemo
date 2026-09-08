1ebcec5cd806d1773964d3cdb7a60793c6a87272
何为开心 <13868626+what-is-happiness@user.noreply.gitee.com>
2026-09-08 09:24:27 +0800

    忽略上传.mvn文件夹

---
+++ src/test/java/com/demo/xh_crawlerdemo/XhCrawlerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.xh_crawlerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XhCrawlerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
